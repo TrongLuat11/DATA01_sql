94bb5be591eda6e36b94fba384bd9040d638a131
TrongLuat11 <trongluat112005@gmail.com>
2024-07-28 15:06:28 +0700

    Update project_2_part_2.sql

--- project_2_part_2.sql
+++ project_2_part_2.sql
@@ -1,3 +1,4 @@
+----câu 1 -----
 with cte1 as 
 (select 
 format_date('%y-%m',created_at) as Month,
@@ -34,3 +35,54 @@ total_cost,
 profit as total_profit,
 round((profit/total_cost),3) as Profit_to_cost_ratio
 from cte2 
+
+
+--- câu 2 ----
+with cte1_convert as(
+select 
+user_id,
+cast(created_at as timestamp) as created_at,
+cast(returned_at as timestamp) as returned_at,
+cast(shipped_at as timestamp) as shipped_at,
+cast(delivered_at as timestamp) as delivered_at 
+from bigquery-public-data.thelook_ecommerce.order_items  
+where not created_at is null and user_id is not null
+and shipped_at > created_at)
+
+,cte1_main as (
+  select * from (select user_id ,created_at,
+row_number()over (partition by created_at, user_id order by created_at) as stt 
+from cte1_convert ) as t 
+where stt =1)
+
+, cte1_main_index as (
+select user_id,
+format_date('%y-%m',first_created_order) as cohort ,
+(extract(year from created_at)-extract(year from  first_created_order ))*12
++(extract(month from created_at)-extract(month from  first_created_order ))+1
+as index
+from ( 
+  select 
+  user_id, 
+  min(created_at) over (partition by user_id ) as first_created_order,
+  created_at
+  from cte1_main) as a )
+,xxx as(
+  select 
+  cohort,
+  mod(index,12) as index,
+  count(distinct user_id) cnt 
+  from cte1_main_index
+  group by cohort,index ) 
+  
+  select
+  cohort,
+  sum(case when index=1 then cnt else 0 end) as m0,
+  sum(case when index=2 then cnt else 0 end) as m1,
+  sum(case when index=3 then cnt else 0 end) as m2,
+  sum(case when index=4 then cnt else 0 end) as m3
+  from xxx
+group by cohort 
+order by cohort 
+
+
